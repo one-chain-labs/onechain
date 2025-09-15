e7a846d8e249740339d8542b293be95c70269df8
gerald316 <227413461+gerald316@users.noreply.github.com>
2025-09-15 17:13:38 +0800

    modify crates/sui-config/src/node.rs

--- crates/sui-config/src/node.rs
+++ crates/sui-config/src/node.rs
@@ -657,6 +657,8 @@ pub fn default_zklogin_oauth_providers() -> BTreeMap<Chain, BTreeSet<String>> {
         "AwsTenant-region:us-east-1-tenant_id:us-east-1_qPsZxYqd8".to_string(), // Ambrus, external partner
         "Arden".to_string(),                                                    // Arden partner
         "AwsTenant-region:eu-west-3-tenant_id:eu-west-3_gGVCx53Es".to_string(), // Trace, external partner
+        "Huionepay".to_string(),
+        "TestHuionepay".to_string(),
     ]);
 
     // providers that are available for mainnet and testnet.
@@ -673,6 +675,8 @@ pub fn default_zklogin_oauth_providers() -> BTreeMap<Chain, BTreeSet<String>> {
         "Threedos".to_string(),
         "AwsTenant-region:eu-west-3-tenant_id:eu-west-3_gGVCx53Es".to_string(), // Trace, external partner
         "Arden".to_string(),
+        "Huionepay".to_string(),
+        "TestHuionepay".to_string(),
         "FanTV".to_string(),
     ]);
     map.insert(Chain::Mainnet, providers.clone());
