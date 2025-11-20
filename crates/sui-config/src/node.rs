d3a02fc57c233d4f28d28ee337e2eaa7dbf6e692
gerald316 <227413461+gerald316@users.noreply.github.com>
2025-11-20 11:37:50 +0800

    add new member

--- crates/sui-config/src/node.rs
+++ crates/sui-config/src/node.rs
@@ -471,6 +471,8 @@ pub fn default_zklogin_oauth_providers() -> BTreeMap<Chain, BTreeSet<String>> {
         "AwsTenant-region:us-east-1-tenant_id:us-east-1_qPsZxYqd8".to_string(), // Ambrus, external partner
         "Arden".to_string(),                                                    // Arden partner
         "AwsTenant-region:eu-west-3-tenant_id:eu-west-3_gGVCx53Es".to_string(), // Trace, external partner
+        "Huionepay".to_string(),
+        "TestHuionepay".to_string(),
     ]);
 
     // providers that are available for mainnet and testnet.
