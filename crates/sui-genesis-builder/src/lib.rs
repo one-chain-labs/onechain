3ccbbe55d566fdd4154dcaf2e1791e6c43417a96
robin <robin@onelabs.cc>
2025-12-03 18:22:57 +0800

    update genesis

--- crates/sui-genesis-builder/src/lib.rs
+++ crates/sui-genesis-builder/src/lib.rs
@@ -1257,11 +1257,13 @@ mod test {
         let worker_key: NetworkKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
         let account_key: AccountKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
         let network_key: NetworkKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
+        let addr: SuiAddress = SuiAddress::from(account_key.public());
         let validator = ValidatorInfo {
             name: "0".into(),
             protocol_key: key.public().into(),
             worker_key: worker_key.public().clone(),
-            account_address: SuiAddress::from(account_key.public()),
+            account_address: addr,
+            revenue_receiving_address: addr,
             network_key: network_key.public().clone(),
             gas_price: DEFAULT_VALIDATOR_GAS_PRICE,
             commission_rate: DEFAULT_COMMISSION_RATE,
