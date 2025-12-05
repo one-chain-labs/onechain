886f95849432a51a70aa85831eead1f37c121958
robin <robin@onelabs.cc>
2025-12-05 17:55:11 +0800

    update sui -> one

--- crates/sui-move/src/new.rs
+++ crates/sui-move/src/new.rs
@@ -7,7 +7,7 @@ use clap::Parser;
 use move_cli::base::new;
 use move_package::source_package::layout::SourcePackageLayout;
 
-const SUI_PKG_NAME: &str = "Sui";
+const SUI_PKG_NAME: &str = "One";
 
 // Use testnet by default. Probably want to add options to make this configurable later
 const SUI_PKG_PATH: &str = "{ git = \"https://github.com/one-chain-labs/onechain.git\", subdir = \"crates/sui-framework/packages/one-framework\", rev = \"framework/testnet\" }";
