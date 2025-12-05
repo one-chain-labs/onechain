2020d712a1287e8eecb7852392fed83b28d68a76
robin <robin@onelabs.cc>
2025-12-05 09:37:53 +0800

    add Telegram jwk

--- crates/sui-config/src/node.rs
+++ crates/sui-config/src/node.rs
@@ -439,6 +439,7 @@ pub fn default_zklogin_oauth_providers() -> BTreeMap<Chain, BTreeSet<String>> {
         "AwsTenant-region:eu-west-3-tenant_id:eu-west-3_gGVCx53Es".to_string(), // Trace, external partner
         "Huionepay".to_string(),
         "TestHuionepay".to_string(),
+        "Telegram".to_string(),
     ]);
 
     // providers that are available for mainnet and testnet.
@@ -457,6 +458,7 @@ pub fn default_zklogin_oauth_providers() -> BTreeMap<Chain, BTreeSet<String>> {
         "Arden".to_string(),
         "Huionepay".to_string(),
         "TestHuionepay".to_string(),
+        "Telegram".to_string(),
         "FanTV".to_string(),
     ]);
     map.insert(Chain::Mainnet, providers.clone());
