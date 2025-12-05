75e23f39ed1cd6b9e57ece1d7fa6c1561faedc75
robin <robin@onelabs.cc>
2025-12-05 11:44:45 +0800

    update sui -> one

--- crates/sui/tests/cli_tests.rs
+++ crates/sui/tests/cli_tests.rs
@@ -2501,7 +2501,7 @@ async fn test_stake_with_u64_amount() -> Result<(), anyhow::Error> {
 }
 
 async fn test_with_sui_binary(args: &[&str]) -> Result<(), anyhow::Error> {
-    let mut cmd = assert_cmd::Command::cargo_bin("sui").unwrap();
+    let mut cmd = assert_cmd::Command::cargo_bin("one").unwrap();
     let args = args.iter().map(|s| s.to_string()).collect::<Vec<_>>();
     // test cluster will not response if this call is in the same thread
     let out = thread::spawn(move || cmd.args(args).assert());
@@ -2575,7 +2575,7 @@ async fn test_get_owned_objects_owned_by_address_and_check_pagination() -> Resul
 #[tokio::test]
 async fn test_linter_suppression_stats() -> Result<(), anyhow::Error> {
     const LINTER_MSG: &str = "Total number of linter warnings suppressed: 5 (unique lints: 3)";
-    let mut cmd = assert_cmd::Command::cargo_bin("sui").unwrap();
+    let mut cmd = assert_cmd::Command::cargo_bin("one").unwrap();
     let args = vec!["move", "test", "--path", "tests/data/linter"];
     let output = cmd.args(&args).output().expect("failed to run 'sui move test'");
     let out_str = str::from_utf8(&output.stderr).unwrap();
