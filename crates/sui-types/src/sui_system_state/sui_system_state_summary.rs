2bfe90dd78975b4872c19ded3008ea6d61b0a486
gerald316 <227413461+gerald316@users.noreply.github.com>
2025-12-23 10:53:41 +0800

    update code

--- crates/sui-types/src/sui_system_state/sui_system_state_summary.rs
+++ crates/sui-types/src/sui_system_state/sui_system_state_summary.rs
@@ -22,9 +22,14 @@ use crate::{
     sui_system_state::get_validator_from_table,
 };
 
-/// This is the JSON-RPC type for the SUI system state object.
+/// This is the JSON-RPC type for the OCT system state object.
 /// It flattens all fields to make them top-level fields such that it as minimum
-/// dependencies to the internal data structures of the SUI system state type.
+/// dependencies to the internal data structures of the OCT system state type.
+#[derive(Default, Debug, Serialize, Deserialize, Clone, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SuiSupperCommitteeSummary {
+    pub proposal_list: Vec<ObjectID>,
+}
 
 #[serde_as]
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
@@ -122,7 +127,7 @@ pub struct SuiSystemStateSummary {
     pub validator_low_stake_grace_period: u64,
 
     // Stake subsidy information
-    /// Balance of SUI set aside for stake subsidies that will be drawn down over time.
+    /// Balance of OCT set aside for stake subsidies that will be drawn down over time.
     #[schemars(with = "BigInt<u64>")]
     #[serde_as(as = "Readable<BigInt<u64>, _>")]
     pub stake_subsidy_balance: u64,
@@ -144,6 +149,9 @@ pub struct SuiSystemStateSummary {
     /// period. Expressed in basis points.
     pub stake_subsidy_decrease_rate: u16,
 
+    // Supper committee
+    pub supper_committee: SuiSupperCommitteeSummary,
+
     // Validator set
     /// Total amount of stake from all active validators at the beginning of the epoch.
     #[schemars(with = "BigInt<u64>")]
@@ -162,7 +170,7 @@ pub struct SuiSystemStateSummary {
     #[schemars(with = "Vec<BigInt<u64>>")]
     #[serde_as(as = "Vec<Readable<BigInt<u64>, _>>")]
     pub pending_removals: Vec<u64>,
-    /// ID of the object that maps from staking pool's ID to the sui address of a validator.
+    /// ID of the object that maps from staking pool's ID to the OneChain address of a validator.
     pub staking_pool_mappings_id: ObjectID,
     /// Number of staking pool mappings.
     #[schemars(with = "BigInt<u64>")]
@@ -186,6 +194,9 @@ pub struct SuiSystemStateSummary {
     pub at_risk_validators: Vec<(SuiAddress, u64)>,
     /// A map storing the records of validator reporting each other.
     pub validator_report_records: Vec<(SuiAddress, Vec<SuiAddress>)>,
+
+    pub trusted_validators: Vec<SuiAddress>,
+    pub only_trusted_validator: bool,
 }
 
 impl SuiSystemStateSummary {
@@ -221,7 +232,7 @@ impl SuiSystemStateSummary {
     }
 }
 
-/// This is the JSON-RPC type for the SUI validator. It flattens all inner structures
+/// This is the JSON-RPC type for the OCT validator. It flattens all inner structures
 /// to top-level fields so that they are decoupled from the internal definitions.
 #[serde_as]
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
@@ -269,6 +280,8 @@ pub struct SuiValidatorSummary {
     #[schemars(with = "BigInt<u64>")]
     #[serde_as(as = "Readable<BigInt<u64>, _>")]
     pub voting_power: u64,
+    pub revenue_receiving_address: SuiAddress,
+    pub only_validator_staking: bool,
     pub operation_cap_id: ObjectID,
     #[schemars(with = "BigInt<u64>")]
     #[serde_as(as = "Readable<BigInt<u64>, _>")]
@@ -297,7 +310,7 @@ pub struct SuiValidatorSummary {
     #[schemars(with = "Option<BigInt<u64>>")]
     #[serde_as(as = "Option<Readable<BigInt<u64>, _>>")]
     pub staking_pool_deactivation_epoch: Option<u64>,
-    /// The total number of SUI tokens in this pool.
+    /// The total number of OCT tokens in this pool.
     #[schemars(with = "BigInt<u64>")]
     #[serde_as(as = "Readable<BigInt<u64>, _>")]
     pub staking_pool_oct_balance: u64,
@@ -369,6 +382,9 @@ impl Default for SuiSystemStateSummary {
             validator_candidates_size: 0,
             at_risk_validators: vec![],
             validator_report_records: vec![],
+            supper_committee: SuiSupperCommitteeSummary::default(),
+            trusted_validators: vec![],
+            only_trusted_validator: true,
         }
     }
 }
@@ -398,6 +414,8 @@ impl Default for SuiValidatorSummary {
             next_epoch_primary_address: None,
             next_epoch_worker_address: None,
             voting_power: 0,
+            revenue_receiving_address: SuiAddress::default(),
+            only_validator_staking: true,
             operation_cap_id: ObjectID::ZERO,
             gas_price: 0,
             commission_rate: 0,
