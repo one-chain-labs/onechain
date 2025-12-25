c0963f703dcbebf122d3036efc64e20857996ad5
gerald316 <227413461+gerald316@users.noreply.github.com>
2025-12-25 15:02:49 +0800

    change name

--- crates/sui-indexer-alt-graphql/src/api/types/validator.rs
+++ crates/sui-indexer-alt-graphql/src/api/types/validator.rs
@@ -198,7 +198,7 @@ impl Validator {
 
     /// The total number of SUI tokens in this pool.
     async fn staking_pool_oct_balance(&self) -> Option<BigInt> {
-        Some(BigInt::from(self.validator().staking_pool.sui_balance))
+        Some(BigInt::from(self.validator().staking_pool.oct_balance))
     }
 
     /// The epoch stake rewards will be added here at the end of each epoch.
