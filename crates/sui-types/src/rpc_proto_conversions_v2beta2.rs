33e26b86ebba82e1ec96970afc1ffd096c2fae7f
gerald316 <227413461+gerald316@users.noreply.github.com>
2025-12-27 11:20:37 +0800

    update code

--- crates/sui-types/src/rpc_proto_conversions_v2beta2.rs
+++ crates/sui-types/src/rpc_proto_conversions_v2beta2.rs
@@ -672,7 +672,7 @@ impl From<crate::sui_system_state::sui_system_state_inner_v1::StakingPoolV1> for
             pool_token_balance,
             exchange_rates,
             pending_stake,
-            pending_total_oct_withdraw,
+            pending_total_sui_withdraw,
             pending_pool_token_withdraw,
             extra_fields,
         }: crate::sui_system_state::sui_system_state_inner_v1::StakingPoolV1,
@@ -686,7 +686,7 @@ impl From<crate::sui_system_state::sui_system_state_inner_v1::StakingPoolV1> for
         message.pool_token_balance = Some(pool_token_balance);
         message.exchange_rates = Some(exchange_rates.into());
         message.pending_stake = Some(pending_stake);
-        message.pending_total_oct_withdraw = Some(pending_total_oct_withdraw);
+        message.pending_total_sui_withdraw = Some(pending_total_sui_withdraw);
         message.pending_pool_token_withdraw = Some(pending_pool_token_withdraw);
         message.extra_fields = Some(extra_fields.into());
         message
