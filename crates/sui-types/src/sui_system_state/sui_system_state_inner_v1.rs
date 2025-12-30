f77333457e35efb0a7df81f7ccdd3b124bd5c199
robin <robin@onelabs.cc>
2025-12-30 19:19:48 +0800

    update

--- crates/sui-types/src/sui_system_state/sui_system_state_inner_v1.rs
+++ crates/sui-types/src/sui_system_state/sui_system_state_inner_v1.rs
@@ -517,15 +517,6 @@ impl SuiSystemStateTrait for SuiSystemStateInnerV1 {
         self.safe_mode
     }
 
-    fn safe_mode_gas_cost_summary(&self) -> GasCostSummary {
-        GasCostSummary {
-            computation_cost: self.safe_mode_computation_rewards.value(),
-            storage_cost: self.safe_mode_storage_rewards.value(),
-            storage_rebate: self.safe_mode_storage_rebates,
-            non_refundable_storage_fee: self.safe_mode_non_refundable_storage_fee,
-        }
-    }
-
     fn advance_epoch_safe_mode(&mut self, params: &AdvanceEpochParams) {
         self.epoch = params.epoch;
         self.safe_mode = true;
